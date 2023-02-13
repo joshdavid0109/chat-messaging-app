91f483a980f0a8d981616e7ea13fc95bd76981ea
xdfranz <darrenjdm0831@gmail.com>
2023-02-13 22:26:18 +0800

    program still work kahit may nag quit na user, to-do edit user profile tuloy ko tomorrow

--- src/LoginHandler.java
+++ src/LoginHandler.java
@@ -1,22 +1,30 @@
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
 public class LoginHandler extends Thread {
-    Socket socket = null;
-    PrintWriter printWriter = null;
-    BufferedReader bufferedReader;
+    Socket socket;
+    final PrintWriter printWriter;
+    final BufferedReader bufferedReader;
     boolean loginStatus;
+
 
     public LoginHandler(Socket clientSocket, PrintWriter printWriter, BufferedReader bufferedReader) {
         this.socket = clientSocket;
@@ -49,134 +57,207 @@ public class LoginHandler extends Thread {
         }
     }
 
-    private void joinServer(String name, NodeList usersList) {
+    private void joinServer(String name, NodeList usersList) throws ParserConfigurationException, IOException, SAXException, TransformerException {
         String message, recipient;
 
         try {
             broadcast(name + " joined the chat");
             messagePrompt(name);
 
-            while ((message = bufferedReader.readLine()) != null) {
+            boolean exit = false;
 
+            while (!exit && (message = bufferedReader.readLine()) != null) {
 
-                if (message.startsWith("/pm")) {
-                    printWriter.println("Send to: ");
-                    recipient = bufferedReader.readLine();
+                if (message.startsWith("/")) {
+                    String[] words = message.split("/");
 
-                    messagePrompt(name);
-                    message = bufferedReader.readLine();
+                    String command = words[1];
+                    System.out.println(command);
+                    switch (command) {
+                        case "edit":
+                            showEditMenu();
+                            printWriter.println("Input your choice: ");
+                            byte choice = Byte.parseByte(bufferedReader.readLine());
+                            switch (choice) {
+                                //TODO
+                                case 1 -> changeUName(name, usersList);
+                                case 2 -> System.out.println();
+                                case 3 -> System.out.println();
+                                case 4 -> System.out.println();
+                            }
+                            break;
+                        case "pm":
+                            printWriter.println("Send to: ");
+                            recipient = bufferedReader.readLine();
+                            messagePrompt(name);
+                            message = bufferedReader.readLine();
+                            for (Map.Entry<String, User> hash : Server.loggedInUserHashMap.entrySet()) {
+                                if (hash.getValue().name().equals(recipient)) {
+                                    for (LoginHandler loginHandler : Server.loginHandlerArraylist) {
+                                        if (loginHandler.socket.getRemoteSocketAddress().toString().equals(hash.getKey()))
+                                            loginHandler.sendMessage(name + ": " + message);
+                                        else
+                                            sendMessage("User not existing");
+                                    }
+                                }
+                            }
 
-                        for (Map.Entry<String, User> hash : Server.loggedInUserHashMap.entrySet()) {
-                             if (hash.getValue().name().equals(recipient)) {
-                                 for (LoginHandler loginHandler : Server.loginHandlerArraylist) {
-                                     if (loginHandler.socket.getRemoteSocketAddress().toString().equals(hash.getKey()))
-                                         loginHandler.sendMessage(name + ": " + message);
-                                     else
-                                         sendMessage("User not existing");
-                                 }
-                             }
-                        }
-
-                } else if (message.startsWith("/quit")) {
-                    socket.close();
-                } else
+                            break;
+                        case "help":
+                            showCommands();
+                            break;
+                        case "quit":
+                            broadcast(name + " has left the chat.");
+                            exit = true;
+                            break;
+                        case "ban":
+                            //TODO
+                            System.out.println();
+                            break;
+                        default:
+                            printWriter.println("command not recognized. input '/help' for a list of commands");
+                    }
+                } else {
                     broadcast(name + ": " + message);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            socket.close();
         }
     }
-
     private void messagePrompt(String name) {
         printWriter.println("\n" + name + ": ");
     }
 
-    /*private void joinServer (String name) throws IOException {
-        sendMessage(name + " joined the chat");
-        String message;
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    private void showCommands() {
+        printWriter.println("\n\n");
+        printWriter.println("COMMANDS");
+        printWriter.println("/help");
+        printWriter.println("/edit");
+        printWriter.println("/privatemessage");
+        printWriter.println("\n\n");
+    }
 
-        while (true) {
+    private void showEditMenu() {
+        printWriter.println("\n\n");
+        printWriter.println("[1] Change username");
+        printWriter.println("[2] Change name");
+        printWriter.println("[3] Change age");
+        printWriter.println("[4] Change password");
+        printWriter.println("\n\n");
+    }
 
-            printWriter.println("INPUT YOUR MESSAGE: ");
-            printWriter.flush();
-            message = bufferedReader.readLine();
-            //get sa system yung date, dapat server side siguro ito para consistent
-            LocalDateTime now = LocalDateTime.now();
-            Messages msg = new Messages(name, message);
-            printWriter.println(dtf.format(now));
-            printWriter.println(msg);
-            printWriter.flush();
-        }
+    public void editName(String name, String newName) {
 
-    }*/
-
-
-        public void broadcast (String message){
-            for (LoginHandler loginHandler : Server.loginHandlerArraylist) {
-                if (loginHandler != null && loginStatus) {
-                        loginHandler.sendMessage(message);
-                }
+    }
+    public void broadcast (String message){
+        for (LoginHandler loginHandler : Server.loginHandlerArraylist) {
+            if (loginHandler != null && loginStatus) {
+                loginHandler.sendMessage(message);
             }
         }
+    }
 
-        public void sendMessage (String message){
-            printWriter.println(message);
+    public void sendMessage (String message){
+        printWriter.println(message);
+    }
+
+    public void changeUName(String name, NodeList usersList){
+        File xmlFile = new File("users.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document users = null;
+        try {
+            users = dBuilder.parse(xmlFile);
+        } catch (SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        String nameToChangeTo = "hardcodelol";
+        for (int i = 0; i < usersList.getLength(); i++) {
+            Element u = (Element) usersList.item(i);
+            String nameNode = u.getElementsByTagName("name").item(0).getTextContent();
+            if (nameNode.equals(name)) {
+                u.getElementsByTagName("name").item(0).setTextContent(nameToChangeTo);
+                System.out.println("User "+nameNode+" has changed name to "+nameToChangeTo);
+                break;
+            }
+        }
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = transformerFactory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        DOMSource source = new DOMSource(users);
+        StreamResult result = new StreamResult(xmlFile);
+        try {
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            e.printStackTrace();
         }
 
-        public void userValidation(String name, NodeList users) {
+        System.out.println(xmlFile);
+        System.out.println(source);
+        System.out.println("XML file updated successfully!");
+    }
 
-            String username, password;
-            while (!loginStatus) {
-                try {
-                    printWriter.println("\nUsername: ");
+    public void userValidation(String name, NodeList users) {
 
-                    username = bufferedReader.readLine();
+        String username, password;
+        while (!loginStatus) {
+            try {
+                printWriter.println("\nUsername: ");
+
+                username = bufferedReader.readLine();
 
 
-                    for (int i = 0; i < users.getLength(); i++) {
-                        Element u = (Element) users.item(i);
-                        String uName = u.getElementsByTagName("Username").item(0).getTextContent();
+                for (int i = 0; i < users.getLength(); i++) {
+                    Element u = (Element) users.item(i);
+                    String uName = u.getElementsByTagName("Username").item(0).getTextContent();
 
-                        if (uName.equals(username)) {
-                            for (int j = 0; j < users.getLength(); j++) {
-                                printWriter.println("\nPassword: ");
-                                password = bufferedReader.readLine();
-                                String pass = u.getElementsByTagName("Password").item(0).getTextContent();
-                                String nameNode = u.getElementsByTagName("name").item(0).getTextContent();
+                    if (uName.equals(username)) {
+                        for (int j = 0; j < users.getLength(); j++) {
+                            printWriter.println("\nPassword: ");
+                            password = bufferedReader.readLine();
+                            String pass = u.getElementsByTagName("Password").item(0).getTextContent();
+                            String nameNode = u.getElementsByTagName("name").item(0).getTextContent();
 
-                                if (pass.equals(password)) {
-                                    name = nameNode;
-                                    loginStatus = true;
-                                    Server.loginHandlerArraylist.add(new LoginHandler(socket, printWriter, bufferedReader));
-                                    User user = new User(u.getAttribute("User"), u.getElementsByTagName("name").item(0).getTextContent(), u.getElementsByTagName("Age").item(0).getTextContent(),
-                                            u.getElementsByTagName("Username").item(0).getTextContent(),
-                                            u.getElementsByTagName("Password").item(0).getTextContent());
+                            if (pass.equals(password)) {
+                                name = nameNode;
+                                loginStatus = true;
+                                Server.loginHandlerArraylist.add(new LoginHandler(socket, printWriter, bufferedReader));
+                                User user = new User(u.getAttribute("User"), u.getElementsByTagName("name").item(0).getTextContent(), u.getElementsByTagName("Age").item(0).getTextContent(),
+                                        u.getElementsByTagName("Username").item(0).getTextContent(),
+                                        u.getElementsByTagName("Password").item(0).getTextContent());
 
-                                    if (u.getElementsByTagName("BanStatus").item(0).getTextContent().equalsIgnoreCase("Banned")){
-                                        printWriter.println("Sorry. Your account is currently banned from the system.");
-                                        break;
-                                    }
-                                    Server.loggedInUserHashMap.put(socket.getRemoteSocketAddress().toString(), user);
-                                    System.out.println("Login Successful!");
-
-                                    joinServer(name, users);
-                                    broadcast(name + ": ");
+                                if (u.getElementsByTagName("BanStatus").item(0).getTextContent().equalsIgnoreCase("Banned")) {
+                                    printWriter.println("Sorry. Your account is currently banned from the system.");
                                     break;
                                 }
-                                    printWriter.println("Invalid password.");
+                                Server.loggedInUserHashMap.put(socket.getRemoteSocketAddress().toString(), user);
+                                System.out.println("Login Successful!");
+
+                                joinServer(name, users);
+                                broadcast(name + ": ");
+                                break;
                             }
-                            break;
-                        } else if (i == users.getLength() - 1)
-                            printWriter.println("User is not existing");
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                            printWriter.println("Invalid password.");
+                        }
+                        break;
+                    } else if (i == users.getLength() - 1)
+                        printWriter.println("User is not existing");
                 }
+            } catch (IOException | ParserConfigurationException | SAXException | TransformerException e) {
+                throw new RuntimeException(e);
             }
         }
+    }
 }
-
-
-
-
